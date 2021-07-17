943e6bb0b136e659c2105e32034f503d607c20db
mihirbpi <mihir.borkar@gmail.com>
2021-07-17 16:34:35 -0700

    Got rid of unneeded global vars

--- bruteforcebacktrack.c
+++ bruteforcebacktrack.c
@@ -1,78 +1,66 @@
 #include <stdio.h>
-
-int puzzle_grid[27][3] = {
-               {0, 0, 7}, {6, 2, 0}, {0, 0, 0},
-               {0, 0, 8}, {0, 3, 0}, {6, 0, 0},
-               {0, 9, 2}, {4, 0, 7}, {0, 0, 0},
-
-               {0, 0, 0}, {0, 0, 0}, {2, 0, 0},
-               {0, 0, 0}, {0, 7, 0}, {0, 6, 1},
-               {0, 0, 0}, {0, 0, 0}, {0, 8, 0},
-
-               {0, 3, 0}, {0, 4, 0}, {0, 2, 0},
-               {0, 4, 0}, {0, 6, 9}, {8, 0, 0},
-               {1, 0, 0}, {0, 0, 0}, {0, 4, 7}
-};
+#define N 27
+#define M M
 
 long long int num_backtracks = 0;
 
-void print_puzzle_grid() {
+void print_puzzle_grid(int puzzle_grid[][]) {
   int i;
 
-  for (i = 0; i < 9; i++) {
-      printf("[%d, %d, %d]", puzzle_grid[3*i][0], puzzle_grid[3*i][1], puzzle_grid[3*i][2]);
+  for (i = 0; i < M*M; i++) {
+      printf("[%d, %d, %d]", puzzle_grid[M*i][0], puzzle_grid[M*i][1], puzzle_grid[M*i][2]);
       printf(", ");
-      printf("[%d, %d, %d]", puzzle_grid[3*i + 1][0], puzzle_grid[3*i + 1][1], puzzle_grid[3*i + 1][2]);
+      printf("[%d, %d, %d]", puzzle_grid[M*i + 1][0], puzzle_grid[M*i + 1][1], puzzle_grid[M*i + 1][2]);
       printf(", ");
-      printf("[%d, %d, %d]", puzzle_grid[3*i + 2][0], puzzle_grid[3*i + 2][1], puzzle_grid[3*i + 2][2]);
+      printf("[%d, %d, %d]", puzzle_grid[M*i + 2][0], puzzle_grid[M*i + 2][1], puzzle_grid[M*i + 2][2]);
       printf("\n");
   }
 
 }
 
-int get_element(int i, int j) {
-  return puzzle_grid[3 * i + (j / 3)][j % 3];
+int get_element(int i, int j, int puzzle_grid[N][M]) {
+  return puzzle_grid[M * i + (j / M)][j % M];
 }
 
-void set_element(int i, int j, int e) {
-  puzzle_grid[3 * i + (j / 3)][j % 3] = e;
+void set_element(int i, int j, int e, int puzzle_grid[N][M]) {
+  puzzle_grid[M * i + (j / M)][j % M] = e;
 }
 
-void clear_square(int i, int j) {
-  puzzle_grid[3 * i + (j / 3)][j % 3] = 0;
+void clear_square(int i, int j, int puzzle_grid[N][M]) {
+  puzzle_grid[M * i + (j / M)][j % M] = 0;
 }
 
-int is_empty(int i, int j) {
+int is_empty(int i, int j, int puzzle_grid[N][M]) {
 
-  if(get_element(i, j) == 0){
+  if(get_element(i, j, puzzle_grid) == 0){
     return 1;
   }
 
   return 0;
 }
 
-int is_valid_element(int i, int j, int e) {
+int is_valid_element(int i, int j, int e, int puzzle_grid[N][M]) {
     int y, x;
 
-    for (y = 0; y < 9; y++) {
+    for (y = 0; y < M*M; y++) {
 
-        if(y != i && get_element(y, j) == e) {
+        if(y != i && get_element(y, j, puzzle_grid) == e) {
             return 0;
         }
     }
 
-    for (x = 0; x < 9; x++) {
+    for (x = 0; x < M*M; x++) {
 
-        if(x != j && get_element(i, x) == e) {
+        if(x != j && get_element(i, x, puzzle_grid) == e) {
             return 0;
         }
     }
 
-    for (y = 3 * (i / 3);  y < (3 * (i / 3)) + 3; y++){
+    for (y = M * (i / M);  y < (M * (i / M)) + M; y++){
 
-        for (x = 3 * (j / 3); x < (3 * (j / 3)) + 3; x++){
+        for (x = M * (j / M); x < (M * (j / M)) + M; x++){
 
-            if( x != i && x != j  && get_element(y, x) == e) {
+            if( x != i && x != j  && get_element(y, x, puzzle_grid) == e) {
                 return 0;
             }
 
@@ -81,17 +69,17 @@ int is_valid_element(int i, int j, int e) {
     return 1;
 }
 
-int solve_sudoku() {
+int solve_sudoku(int puzzle_grid[N][M]) {
   int x, y, i, j, guess;
 
   i = -1;
   j = -1;
 
-  for (y = 0; y < 9; y++) {
+  for (y = 0; y < M*M; y++) {
 
-    for (x = 0; x < 9; x++) {
+    for (x = 0; x < M*M; x++) {
 
-      if(is_empty(y, x)) {
+      if(is_empty(y, x, puzzle_grid)) {
         i = y;
         j = x;
       }
@@ -102,15 +90,15 @@ int solve_sudoku() {
         return 1;
   }
 
-  for (guess = 1; guess <= 9; guess++) {
+  for (guess = 1; guess < M*M+1; guess++) {
 
-    if(is_valid_element(i, j, guess)) {
-      set_element(i, j, guess);
+    if(is_valid_element(i, j, guess, puzzle_grid)) {
+      set_element(i, j, guess, puzzle_grid);
 
-      if(solve_sudoku()){
+      if(solve_sudoku(puzzle_grid)){
         return 1;
       } else {
-        clear_square(i, j);
+        clear_square(i, j, puzzle_grid);
         num_backtracks ++;
       }
     }
@@ -119,15 +107,15 @@ int solve_sudoku() {
   return 0;
 }
 
-char *check_puzzle(){
+char *check_puzzle(int puzzle_grid[N][M]){
 
   int i, j;
 
-  for (i = 0; i < 9; i++) {
+  for (i = 0; i < M*M; i++) {
 
-    for (j = 0; j < 9; j++) {
+    for (j = 0; j < M*M; j++) {
 
-      if(is_valid_element(i, j, get_element(i, j)) == 0) {
+      if(is_valid_element(i, j, get_element(i, j, puzzle_grid), puzzle_grid) == 0) {
         return "False";
       }
     }
@@ -136,12 +124,28 @@ char *check_puzzle(){
 }
 
 int main(){
+
+  int puzzle_grid[N][M] = {
+                 {0, 0, 7}, {6, 2, 0}, {0, 0, 0},
+                 {0, 0, 8}, {0, M, 0}, {6, 0, 0},
+                 {0, 9, 2}, {4, 0, 7}, {0, 0, 0},
+
+                 {0, 0, 0}, {0, 0, 0}, {2, 0, 0},
+                 {0, 0, 0}, {0, 7, 0}, {0, 6, 1},
+                 {0, 0, 0}, {0, 0, 0}, {0, 8, 0},
+
+                 {0, M, 0}, {0, 4, 0}, {0, 2, 0},
+                 {0, 4, 0}, {0, 6, 9}, {8, 0, 0},
+                 {1, 0, 0}, {0, 0, 0}, {0, 4, 7}
+  };
+
+
   printf("original puzzle: \n");
-  print_puzzle_grid();
-  solve_sudoku();
+  print_puzzle_grid(puzzle_grid);
+  solve_sudoku(puzzle_grid);
   printf("solved puzzle: \n");
-  print_puzzle_grid();
+  print_puzzle_grid(puzzle_grid);
   printf("backtracks used: %lli \n", num_backtracks);
-  printf("Is the puzzle indeed solved?: %s \n", check_puzzle());
+  printf("Is the puzzle indeed solved?: %s \n", check_puzzle(puzzle_grid));
   return 0;
 }
--- bruteforcebacktrack_withimplic.c
+++ bruteforcebacktrack_withimplic.c
@@ -1,78 +1,65 @@
 #include <stdio.h>
-
-int puzzle_grid[27][3] = {
-               {0, 0, 7}, {6, 2, 0}, {0, 0, 0},
-               {0, 0, 8}, {0, 3, 0}, {6, 0, 0},
-               {0, 9, 2}, {4, 0, 7}, {0, 0, 0},
-
-               {0, 0, 0}, {0, 0, 0}, {2, 0, 0},
-               {0, 0, 0}, {0, 7, 0}, {0, 6, 1},
-               {0, 0, 0}, {0, 0, 0}, {0, 8, 0},
-
-               {0, 3, 0}, {0, 4, 0}, {0, 2, 0},
-               {0, 4, 0}, {0, 6, 9}, {8, 0, 0},
-               {1, 0, 0}, {0, 0, 0}, {0, 4, 7}
-};
+#define N 27
+#define M 3
 
 long long int num_backtracks = 0;
-int implications_list[9][9][10] = {0};
 
-void print_puzzle_grid() {
+void print_puzzle_grid(int puzzle_grid[N][M]) {
   int i;
 
-  for (i = 0; i < 9; i++) {
-      printf("[%d, %d, %d]", puzzle_grid[3*i][0], puzzle_grid[3*i][1], puzzle_grid[3*i][2]);
+  for (i = 0; i < M*M; i++) {
+      printf("[%d, %d, %d]", puzzle_grid[M*i][0], puzzle_grid[M*i][1], puzzle_grid[M*i][2]);
       printf(", ");
-      printf("[%d, %d, %d]", puzzle_grid[3*i + 1][0], puzzle_grid[3*i + 1][1], puzzle_grid[3*i + 1][2]);
+      printf("[%d, %d, %d]", puzzle_grid[M*i + 1][0], puzzle_grid[M*i + 1][1], puzzle_grid[M*i + 1][2]);
       printf(", ");
-      printf("[%d, %d, %d]", puzzle_grid[3*i + 2][0], puzzle_grid[3*i + 2][1], puzzle_grid[3*i + 2][2]);
+      printf("[%d, %d, %d]", puzzle_grid[M*i + 2][0], puzzle_grid[M*i + 2][1], puzzle_grid[M*i + 2][2]);
       printf("\n");
   }
 }
 
-int get_element(int i, int j) {
-  return puzzle_grid[3 * i + (j / 3)][j % 3];
+int get_element(int i, int j, int puzzle_grid[N][M]) {
+  return puzzle_grid[M * i + (j / M)][j % M];
 }
 
-void set_element(int i, int j, int e) {
-  puzzle_grid[3 * i + (j / 3)][j % 3] = e;
+void set_element(int i, int j, int e, int puzzle_grid[N][M]) {
+  puzzle_grid[M * i + (j / M)][j % M] = e;
 }
 
-void clear_square(int i, int j) {
-  puzzle_grid[3 * i + (j / 3)][j % 3] = 0;
+void clear_square(int i, int j, int puzzle_grid[N][M]) {
+  puzzle_grid[M * i + (j / M)][j % M] = 0;
 }
 
-int is_empty(int i, int j) {
+int is_empty(int i, int j, int puzzle_grid[N][M]) {
 
-  if(get_element(i, j) == 0){
+  if(get_element(i, j, puzzle_grid) == 0){
     return 1;
   }
 
   return 0;
 }
 
-int is_valid_element(int i, int j, int e) {
+int is_valid_element(int i, int j, int e, int puzzle_grid[N][M]) {
     int y, x;
 
-    for (y = 0; y < 9; y++) {
+    for (y = 0; y < M*M; y++) {
 
-        if(y != i && get_element(y, j) == e) {
+        if(y != i && get_element(y, j, puzzle_grid) == e) {
             return 0;
         }
     }
 
-    for (x = 0; x < 9; x++) {
+    for (x = 0; x < M*M; x++) {
 
-        if(x != j && get_element(i, x) == e) {
+        if(x != j && get_element(i, x, puzzle_grid) == e) {
             return 0;
         }
     }
 
-    for (y = 3 * (i / 3);  y < (3 * (i / 3)) + 3; y++) {
+    for (y = M * (i / M);  y < (M * (i / M)) + M; y++) {
 
-        for (x = 3 * (j / 3); x < (3 * (j / 3)) + 3; x++) {
+        for (x = M * (j / M); x < (M * (j / M)) + M; x++) {
 
-            if( x != i && x != j  && get_element(y, x) == e) {
+            if( x != i && x != j  && get_element(y, x, puzzle_grid) == e) {
                 return 0;
             }
         }
@@ -80,25 +67,25 @@ int is_valid_element(int i, int j, int e) {
     return 1;
 }
 
-void make_implications() {
+void make_implications(int puzzle_grid[N][M], int implications_list[M*M][M*M][M*M+1]) {
   int i, j, k, num, num_nonzeroes, guess;
 
-  for (i = 0; i < 9; i++) {
+  for (i = 0; i < M*M; i++) {
 
-    for (j = 0; j < 9; j++) {
+    for (j = 0; j < M*M; j++) {
 
-        if (is_empty(i, j)) {
+        if (is_empty(i, j, puzzle_grid)) {
 
-          for (num = 1; num < 10; num++){
+          for (num = 1; num < M*M+1; num++){
 
-            if(is_valid_element(i, j, num) == 0){
+            if(is_valid_element(i, j, num, puzzle_grid) == 0){
               implications_list[i][j][num] = 0;
             }
           }
 
-          num_nonzeroes = 9;
+          num_nonzeroes = M*M;
 
-          for (k = 1; k < 10; k++) {
+          for (k = 1; k < M*M+1; k++) {
 
             if(implications_list[i][j][k] == 0){
                 num_nonzeroes --;
@@ -109,48 +96,48 @@ void make_implications() {
 
           if(num_nonzeroes == 1) {
             implications_list[i][j][0] = 1;
-            set_element(i, j, guess);
+            set_element(i, j, guess, puzzle_grid);
           }
       }
     }
   }
 }
 
-void clear_implications() {
+void clear_implications(int puzzle_grid[N][M], int implications_list[M*M][M*M][M*M+1]) {
   int i, j, k;
 
-  for (i = 0; i < 9; i++) {
+  for (i = 0; i < M*M; i++) {
 
-    for (j = 0; j < 9; j++) {
+    for (j = 0; j < M*M; j++) {
 
       if(implications_list[i][j][0] == 1){
-        clear_square(i, j);
+        clear_square(i, j, puzzle_grid);
       }
     }
   }
 
-  for (i = 0; i < 10; i++) {
+  for (i = 0; i < M*M; i++) {
 
-    for (j = 0; j < 10; j++) {
+    for (j = 0; j < M*M; j++) {
 
-      for (k = 0; k < 10; k++){
+      for (k = 0; k < M*M+1; k++){
         implications_list[i][j][k] = k;
       }
     }
   }
 }
 
-int solve_sudoku() {
+int solve_sudoku(int puzzle_grid[N][M], int implications_list[M*M][M*M][M*M+1]) {
   int x, y, i, j, guess;
 
   i = -1;
   j = -1;
 
-  for (y = 0; y < 9; y++) {
+  for (y = 0; y < M*M; y++) {
 
-    for (x = 0; x < 9; x++) {
+    for (x = 0; x < M*M; x++) {
 
-      if(is_empty(y, x)) {
+      if(is_empty(y, x, puzzle_grid)) {
         i = y;
         j = x;
       }
@@ -161,17 +148,17 @@ int solve_sudoku() {
         return 1;
   }
 
-  for (guess = 1; guess <= 9; guess++) {
+  for (guess = 1; guess < M*M+1; guess++) {
 
-    if(is_valid_element(i, j, guess)) {
-      set_element(i, j, guess);
-      make_implications();
+    if(is_valid_element(i, j, guess, puzzle_grid)) {
+      set_element(i, j, guess, puzzle_grid);
+      make_implications(puzzle_grid, implications_list);
 
-      if(solve_sudoku()){
+      if(solve_sudoku(puzzle_grid, implications_list)){
         return 1;
       } else {
-        clear_implications();
-        clear_square(i, j);
+        clear_implications(puzzle_grid, implications_list);
+        clear_square(i, j, puzzle_grid);
         num_backtracks ++;
       }
     }
@@ -179,15 +166,15 @@ int solve_sudoku() {
   return 0;
 }
 
-char *check_puzzle(){
+char *check_puzzle(int puzzle_grid[N][M]){
 
   int i, j;
 
-  for (i = 0; i < 9; i++) {
+  for (i = 0; i < M*M; i++) {
 
-    for (j = 0; j < 9; j++) {
+    for (j = 0; j < M*M; j++) {
 
-      if(is_valid_element(i, j, get_element(i, j)) == 0) {
+      if(is_valid_element(i, j, get_element(i, j, puzzle_grid), puzzle_grid) == 0) {
         return "False";
       }
     }
@@ -196,23 +183,41 @@ char *check_puzzle(){
 }
 
 int main(){
+
+  int puzzle_grid[N][M] = {
+                 {0, 0, 7}, {6, 2, 0}, {0, 0, 0},
+                 {0, 0, 8}, {0, 3, 0}, {6, 0, 0},
+                 {0, 9, 2}, {4, 0, 7}, {0, 0, 0},
+
+                 {0, 0, 0}, {0, 0, 0}, {2, 0, 0},
+                 {0, 0, 0}, {0, 7, 0}, {0, 6, 1},
+                 {0, 0, 0}, {0, 0, 0}, {0, 8, 0},
+
+                 {0, 3, 0}, {0, 4, 0}, {0, 2, 0},
+                 {0, 4, 0}, {0, 6, 9}, {8, 0, 0},
+                 {1, 0, 0}, {0, 0, 0}, {0, 4, 7}
+  };
+
+  int implications_list[M*M][M*M][M*M+1] = {0};
+
   int i, j, k;
 
-  for (i = 0; i < 10; i++) {
+  for (i = 0; i < M*M; i++) {
 
-    for (j = 0; j < 10; j++) {
+    for (j = 0; j < M*M; j++) {
 
-      for (k = 0; k <= 9; k++) {
+      for (k = 0; k < M*M+1; k++) {
         implications_list[i][j][k] = k;
       }
     }
   }
+
   printf("original puzzle: \n");
-  print_puzzle_grid();
-  solve_sudoku();
+  print_puzzle_grid(puzzle_grid);
+  solve_sudoku(puzzle_grid, implications_list);
   printf("solved puzzle: \n");
-  print_puzzle_grid();
+  print_puzzle_grid(puzzle_grid);
   printf("backtracks used: %lli \n", num_backtracks);
-  printf("Is the puzzle indeed solved?: %s \n", check_puzzle());
+  printf("Is the puzzle indeed solved?: %s \n", check_puzzle(puzzle_grid));
   return 0;
 }
